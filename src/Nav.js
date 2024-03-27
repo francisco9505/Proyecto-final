d3d0103d818f644c354711c3bd112efce8815895
Francisco Castillo <franciscocastillo@MacBook-Air-de-Francisco.local>
2024-03-27 13:46:19 +0100

    nuevas cosas

---
+++ src/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Nav = () => {
+  return (
+    <>
+      <ul>
+        <li>
+          {" "}
+          <img src="https://play-lh.googleusercontent.com/t_XvMjLc2uxTVXMiQatnBGbmRDuu_6-5cbPzJJhyaPyBh4iPAqqJ2MwOo0JAxScRwUqL"></img>
+        </li>
+        <li><a>HOME</a></li>
+        <li><a>ABOUT</a></li>
+        <li><a>MENU</a></li>
+        <li><a>RESERVATIONS</a></li>
+        <li><a>ORDER ONLINE</a></li>
+        <li><a>LOGIN</a></li>
+      </ul>
+    </>
+  );
+};
